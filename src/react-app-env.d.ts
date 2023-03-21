ac34306bb923bfd49d9085cac24760bfdb590df6
Farhan Hamdallah <fhbluebeast356@gmail.com>
2023-03-22 01:25:06 +0700

    add config const

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'test'
+      API_URL: 'http://127.0.0.1:8080'
+      }
+  }
+  interface Window {
+      Stripe: any
+  }
+  
